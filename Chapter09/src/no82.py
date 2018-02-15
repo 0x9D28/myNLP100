4e5836b8b4e52f728692a5946476a92088415e80
Inowe2457 <byawell@gmail.com>
2018-02-15 10:33:52 -0800

    complete 82

---
+++ Chapter09/src/no82.py
@@ -0,0 +1,25 @@
+'''
+82. 文脈の抽出
+81で作成したコーパス中に出現するすべての単語tに関して，
+単語tと文脈語cのペアをタブ区切り形式ですべて書き出せ．
+ただし，文脈語の定義は次の通りとする．
+・ある単語tの前後d単語を文脈語cとして抽出する
+  （ただし，文脈語に単語tそのものは含まない）
+・単語tを選ぶ度に，文脈幅dは{1,2,3,4,5}の範囲でランダムに決める．
+'''
+import sys
+import random
+
+infile = open(sys.argv[1], 'rt')
+outfile = open(sys.argv[2], 'wt')
+
+for j, line in enumerate(infile):
+    line = line.split()
+    for i, token in enumerate(line):
+        window_size = random.choice(range(1, 6))
+        context = line[i-window_size:i] + line[i+1:i+window_size+1]
+        outfile.write("{}\t{}\n".format(token, " ".join(context)))
+    if j % 100000 == 0:
+        print("iter: {}".format(j))
+infile.close()
+outfile.close()
