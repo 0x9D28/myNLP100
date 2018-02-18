4dd9e256e309f3a9a3cae38c89dd8956cde794d2
Inowe2457 <byawell@gmail.com>
2018-02-17 19:19:54 -0800

    complete 83

--- Chapter09/src/no83.py
+++ Chapter09/src/no83.py
@@ -6,15 +6,14 @@
 ・f(∗,c): 文脈語cの出現回数
 ・N: 単語と文脈語のペアの総出現回数
 
-save (f(t,c), f(t,*), f(*,c), N) tuple as pickle file
+$ python3 no83.py [input no.82 output] [number of no.82 output lines (gotten by wc command)] [necessary input amount assigned percentile (0, 100]] [output directory]
 '''
 import sys
 from collections import defaultdict, Counter
-import pickle
 import os
 
 infile = open(sys.argv[1], 'rt')
-outdir = sys.argv[2]
+outdir = sys.argv[4]
 ftc = defaultdict(Counter)
 ft = Counter()
 fc = Counter()
@@ -23,44 +22,62 @@ N = 0
 if not os.path.exists(outdir):
     os.mkdir(outdir)
 
-for i, line in enumerate(infile):
+length = int(sys.argv[2])  # number of out82.txt lines
+uplimit = float(sys.argv[3])  # use uplimit[%] of out82.txt
+for i, line in enumerate(infile, start=1):
     term, context = line.split("\t")
     context = context.split()
     ftc[term].update(context)
-    if i % 10**6 == 0:
-        print('iter: {} ({}%)'.format(i, round(100*i/120418829, 2)))
-    if i / (10**7) > 1:
-        break
-infile.close()
-print("finished iter for f(t,c)")
-with open(os.path.join(outdir, 'ftc.txt'), 'wt') as f:
-    for word, context in ftc.items():
-        context = ["{}:{}".format(k, v) for k, v in context.items()]
-        f.write("{}\t{}\n".format(word, ",".join(context)))
-del ftc  # because too large data
-print("dump f(t,c)")
-
-infile = open(sys.argv[1], 'rt')
-for i, line in enumerate(infile):
-    term, context = line.split("\t")
-    context = context.split()
     ft.update([term])
     fc.update(context)
     N += len(context)
-    if i % 1000000 == 0:
-        print("iter: {} ({}%)".format(i, round(100*i/120418829, 2)))
-    if i / (10**7) > 1:
+    percentile = round(100 * i / length, 2)
+    if i % 10**6 == 0:
+        print('counting frequency: {}%'.format(percentile))
+    if percentile > uplimit:
         break
 infile.close()
-print("finished iter for f(t,*), f(*,c) and N")
-with open(os.path.join(outdir, 'ft.txt'), 'wt') as f:
-    for word, freq in ft.items():
-        f.write("{}:{}\n".format(word, freq))
-print("written f(t,*)")
-with open(os.path.join(outdir, 'fc.txt'), 'wt') as f:
-    for word, freq in fc.items():
-        f.write("{}:{}\n".format(word, freq))
-print("written f(*,c)")
+print("finished counting")
+
+print("voc size: {}".format(len(ft)))
+
+# write data as txt file
+print("start writing N")
 with open(os.path.join(outdir, 'N.txt'), 'wt') as f:
     f.write(str(N))
 print("written N")
+del N
+
+print("start writing f(t,*)")
+with open(os.path.join(outdir, 'ft.txt'), 'wt') as f:
+    length = len(ft)
+    for i, k_v in enumerate(ft.items(), start=1):
+        word, freq = k_v
+        f.write("{}:{}\n".format(word, freq))
+        if i % 10**5 == 0:
+            print("writing f(t,*): {}%".format(100*i/length))
+print("written f(t,*)")
+del ft
+
+print("start writing f(*,c)")
+with open(os.path.join(outdir, 'fc.txt'), 'wt') as f:
+    length = len(fc)
+    for i, k_v in enumerate(fc.items(), start=1):
+        word, freq = k_v
+        f.write("{}:{}\n".format(word, freq))
+        if i % 10**5 == 0:
+            print("writing f(*,c): {}%".format(100*i/length))
+print("written f(*,c)")
+del fc
+
+print("start writing f(t,c)")
+with open(os.path.join(outdir, 'ftc.txt'), 'wt') as f:
+    length = len(ftc)
+    for i, k_v in enumerate(ftc.items(), start=1):
+        word, context = k_v
+        context = ["{}:{}".format(k, v) for k, v in context.items()]
+        f.write("{}\t{}\n".format(word, ",".join(context)))
+        if i % 10**5 == 0:
+            print("writing f(t,c): {}%".format(100*i/length))
+print("written f(t,c)")
+
