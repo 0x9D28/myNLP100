868efd715a3adf821e68a614ac06f2fbd7ea1f1d
Inowe2457 <byawell@gmail.com>
2018-02-18 22:38:03 -0800

    revise src->idx, and add srcs

--- Chapter05/src/no41.py
+++ Chapter05/src/no41.py
@@ -16,30 +16,40 @@ from no40 import Morph
 import re
 
 class Chunk(object):
-    def __init__(self, chunk_lines):
+    def __init__(self, chunk_lines, headers_lines):
         header = chunk_lines[0].split()
-        self.src = int(header[1])
+        self.idx = int(header[1])
         self.dst = int(header[2].replace("D", ""))
+        srcs = []
+        # import pdb; pdb.set_trace()
+        for header_line in headers_lines:
+            header_line = header_line.split()
+            if self.idx == int(header_line[2].replace("D", "")):
+                srcs.append(int(header_line[1]))
+        self.srcs = srcs
         self.morphs = [Morph(line) for line in chunk_lines[1:]]
     def __repr__(self):
-        bases = "".join([morph.surface for morph in self.morphs])
-        return "src: {}, dst: {}, surface: {}"\
-               .format(self.src, self.dst, bases)
+        surfaces = "".join([morph.surface for morph in self.morphs])
+        srcs = ",".join([str(src) for src in self.srcs]) if self.srcs else "nan"
+        return "idx: {}, srcs: ({}), dst: {}, surface: {}"\
+               .format(self.idx, srcs, self.dst, surfaces)
 
 
 def load_cabocha(lines):
-    sents, sent, chunk_lines = [], [], []
+    sents, sent, chunk_lines, headers = [], [], [], []
     for line in lines:
         if line[0] == "*":
             if chunk_lines:
-                sent.append(Chunk(chunk_lines))
+                sent.append(Chunk(chunk_lines, headers))
+            headers.append(line)
             chunk_lines = [line]
         elif line.rstrip("\n") == "EOS":
             if chunk_lines:
-                sent.append(Chunk(chunk_lines))
+                sent.append(Chunk(chunk_lines, headers))
                 sents.append(sent)
             sent = []
             chunk_lines = []
+            headers = []
         else:
             chunk_lines.append(line)
     return sents
@@ -49,4 +59,5 @@ if __name__ == '__main__':
     f = open(sys.argv[1], 'rt')
     sents = load_cabocha(f)
     f.close()
+    # import pdb; pdb.set_trace()
     print(*sents[8], sep="\n")
