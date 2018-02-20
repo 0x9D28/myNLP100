fa430c6fe51cff7183d87a357143e2e46a27952c
inowe2457 <byawell@gmail.com>
2018-02-20 19:57:33 +0900

    complete 42

---
+++ Chapter05/src/no42.py
@@ -0,0 +1,23 @@
+'''
+42. 係り元と係り先の文節の表示
+係り元の文節と係り先の文節のテキストをタブ区切り形式ですべて抽出せよ．
+ただし，句読点などの記号は出力しないようにせよ．
+'''
+from no41 import Chunk, load_cabocha
+import sys
+
+if __name__ == '__main__':
+    f = open('../data/neko.txt.cabocha', 'rt', encoding='utf8')
+    sents = load_cabocha(f)
+    f.close()
+    # import pdb; pdb.set_trace()
+    print(*sents[8], sep="\n")
+    for sent in sents:
+        sent2 = sent
+        for chunk in sent:
+            for chunk2 in sent2:
+                if chunk.dst == chunk2.idx:
+                    surface = "".join([morph.surface for morph in chunk.morphs])
+                    surface2 = "".join([morph.surface for morph in chunk2.morphs])
+                    print("{}\t{}".format(surface, surface2))
+                    break
