823d00be5781b3dd1ff9b9ec7eb2baf2f3624d1f
inowe2457 <byawell@gmail.com>
2018-02-23 19:21:42 +0900

    make SemanticSpace calss, it has similarity and most_similar methods like gensim.models.Word2Vec

--- Chapter09/src/no85.py
+++ Chapter09/src/no85.py
@@ -7,7 +7,43 @@ import pickle as pkl
 import sys
 from sklearn.decomposition import TruncatedSVD as TSVD
 import numpy as np
-import os
+from numpy import linalg as LA
+
+
+class SemanticSpace(object):
+    def __init__(self, labeled_matrix):
+        self.matrix = labeled_matrix
+
+    def cos_sim(self, vec1, vec2):
+        sim = np.dot(vec1, vec2) / (LA.norm(vec1) * LA.norm(vec2))
+        if sim > 0:
+            return sim
+        else:
+            return 0
+
+    def similarity(self, term1, term2):
+        return self.cos_sim(self.matrix[term1], self.matrix[term2])
+
+    def most_similar(self, positive=[], negative=[], topn=10):
+        sim_dic = {}
+        rslt = []
+        for val in self.matrix.values():
+            Ndim = val.shape[0]
+            break
+        vec_sum = np.zeros(Ndim)
+        for term in positive:
+            vec_sum += self.matrix[term]
+        for term in negative:
+            vec_sum -= self.matrix[term]
+        for term, vec in self.matrix.items():
+            sim_dic[term] = self.cos_sim(vec_sum, vec)
+        for i, t_s in enumerate(sorted(sim_dic.items(),
+                                       key=lambda x: -x[1]), start=1):
+            term, sim = t_s
+            rslt.append((term, sim))
+            if i >= topn:
+                break
+        return rslt
 
 if __name__ == '__main__':
     # SVD
@@ -29,6 +65,6 @@ if __name__ == '__main__':
         X_dic[term] = X[i]
 
     with open(sys.argv[3], 'wb') as f:
-        pkl.dump(X_dic, f)
+        pkl.dump(SemanticSpace(X_dic), f)
     
     
