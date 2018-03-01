679f87773bc15b901d83fac823ad139b3c39ae92
Inowe2457 <byawell@gmail.com>
2018-03-01 04:27:57 -0800

    use the most right particle in the chunk, not left

--- Chapter05/no47.py
+++ Chapter05/no47.py
@@ -24,19 +24,22 @@ import sys
 if __name__ == '__main__':
     infile = open(sys.argv[1], 'rt')
     sents = load_cabocha(infile)
-    infile.close()
     outfile = open(sys.argv[2], 'wt')
     # import pdb; pdb.set_trace()
     for sent in sents:
         sent2 = sent
         for chunk in sent:
+            # want most left verb in chunk
             predicate = search_pos(chunk, pos="動詞", fmt='morph')
             if predicate:
                 particles = []
                 chunk2s = []
                 for chunk2 in sent2:
-                    particle = search_pos(chunk2, "助詞", fmt='morph')
-                    if particle and chunk2.dst == chunk.idx:
+                    # want most right particle in chunk
+                    particle_chunk = search_pos(chunk2, "助詞", fmt='chunk')
+                    if particle_chunk and chunk2.dst == chunk.idx:
+                        particle = [morph for morph in particle_chunk.morphs
+                                    if morph.pos == '助詞'][-1]
                         particles.append(particle)
                         chunk2s.append(chunk2)
                 wo_case_chunk = search_pos(chunk2s, pos="助詞", base="を", fmt='chunk')
@@ -46,5 +49,7 @@ if __name__ == '__main__':
                     del particles[idx]
                     particles = " ".join([particle.base for particle in particles])
                     arguments = " ".join([c.surface for c in chunk2s])
-                    outfile.write("{}\t{}\t{}\n".format(wo_case_chunk.surface+predicate.base, particles, arguments))
+                    outfile.write("{}\t{}\t{}\n".format(
+                        wo_case_chunk.surface+predicate.base, particles, arguments))
+    infile.close()
     outfile.close()
