67ce2e1aa6e75380223b25376d5a06c010f6f424
Inowe2457 <byawell@gmail.com>
2018-03-12 06:55:22 -0700

    rewrite a few lines for applyng to class method

--- Chapter06/no59.py
+++ Chapter06/no59.py
@@ -7,43 +7,53 @@ import xml.etree.ElementTree as ET
 import sys
 
 
-def extract_phrases(string, tag):
-    sexp = parse_sexp(string)
-    
+class SExpressionParser(object):
+    """
+    S-expression example
+     (ROOT (S (PP (NP (JJ Natural) (NN language) (NN processing)) (IN From) (NP (NNP Wikipedia))) (, ,) (NP (NP (DT the) (JJ free) (NN encyclopedia) (JJ Natural) (NN language) (NN processing)) (PRN (-LRB- -LRB-) (NP (NN NLP)) (-RRB- -RRB-))) (VP (VBZ is) (NP (NP (NP (DT a) (NN field)) (PP (IN of) (NP (NN computer) (NN science)))) (, ,) (NP (JJ artificial) (NN intelligence)) (, ,) (CC and) (NP (NP (NNS linguistics)) (VP (VBN concerned) (PP (IN with) (NP (NP (DT the) (NNS interactions)) (PP (IN between) (NP (NP (NNS computers)) (CC and) (NP (JJ human) (-LRB- -LRB-) (JJ natural) (-RRB- -RRB-) (NNS languages)))))))))) (. .)))
+    """
+    def __init__(self, sexp):
+        self.sexp = sexp
 
 
-def parse_sexp(string):
-    """
-    from https://en.wikipedia.org/wiki/S-expression
-    >>> parse_sexp("(+ 5 (+ 3 5))")
-    [['+', '5', ['+', '3', '5']]]
-    """
-    sexp = [[]]
-    word = ''
-    in_str = False
-    for char in string:
-        if char is '(' and not in_str:
-            sexp.append([])
-        elif char is ')' and not in_str:
-            if word:
-                sexp[-1].append(word)
-                word = ''
-            temp = sexp.pop()
-            sexp[-1].append(temp)
-        elif char in (' ', '\n', '\t') and not in_str:
-            if word:
-                sexp[-1].append(word)
-                word = ''
-        elif char is '\"':
-            in_str = not in_str
-        else:
-            word += char
-    return sexp[0]
+    @classmethod
+    def parse_sexp(cls, string):
+        """
+        copy and paste from https://en.wikipedia.org/wiki/S-expression
+        and rewrite a few lines for applying to class method
+        >>> parse_sexp("(+ 5 (+ 3 5))")
+        [['+', '5', ['+', '3', '5']]]
+        """
+        sexp = [[]]
+        word = ''
+        in_str = False
+        for char in string:
+            if char is '(' and not in_str:
+                sexp.append([])
+            elif char is ')' and not in_str:
+                if word:
+                    sexp[-1].append(word)
+                    word = ''
+                temp = sexp.pop()
+                sexp[-1].append(temp)
+            elif char in (' ', '\n', '\t') and not in_str:
+                if word:
+                    sexp[-1].append(word)
+                    word = ''
+            elif char is '\"':
+                in_str = not in_str
+            else:
+                word += char
+        SEparser = cls(sexp[0])
+        return SEparser
+
+    def find_all(self, tag):
+        pass
 
 
 if __name__ == '__main__':
     tree = ET.parse(sys.argv[1])
     root = tree.getroot()
     for sexp in root.iter('parse') :
-        NP_list = extract_phrases(sexp.text, 'NP')
+        NP_list = SExpressionParser.parse_sexp(sexp.text).find_all('NP')
         print(*NP_list, sep='\n')
